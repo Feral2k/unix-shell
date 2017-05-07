80ccbf178d2b8254fa3a5f8eabdf26030bc716da
Logan Garcia <logangarcia@fastmail.fm>
2017-05-07 11:28:45 -0700

    attempted to add a history feature

--- osh.c
+++ osh.c
@@ -17,25 +17,46 @@ Logan Garcia
 
 int main(void) {
 	char *args[MAX_LINE/2 + 1]; /* command line arguments */
+	char *hist[100][MAX_LINE/2 + 1];
+	int index = 0;
 	int should_run = 1; /* flag to determine when to exit program */
+	bool ampersand;
+	char *newline;
+	char *token;
+	int i;
 	
 	while (should_run) {
 		printf("osh>");
-		fflush(stdout);
-		bool ampersand = false;
+		fflush(stdout);		
+		ampersand = false;
 		char tempArgs[100];
 		fgets(tempArgs, sizeof(tempArgs), stdin);
 		
-		char *newline = strchr(tempArgs, '\n');
+		newline = strchr(tempArgs, '\n');
 		if (newline) {
   			*newline = 0;
   		}
 		
-		char* token = strtok(tempArgs, " "); //holds tokenized result		
-		int i = 0;
+		token = strtok(tempArgs, " "); //holds tokenized result		
+		i = 0;
 		
 		if (strcmp(token, "exit") == 0) {
 			should_run = 0;
+		}		
+		
+		else if (strcmp(token, "history") == 0) {
+			if (index == 0) {
+				printf("No commands in history.\n");
+			}
+			else {
+				for (int v = (index - 1); v >= 0; v--) {
+					printf("%i", v + 1);
+					for (int x = 0; hist[v][x] != NULL; x++) {
+						printf(" %s", hist[v][x]);
+					}
+					printf("\n");
+				}
+			}
 		}
 		
 		else {		
@@ -47,33 +68,25 @@ int main(void) {
 				}
 				else {
 					args[i] = token;
-					printf("token: %s\n", args[i]);
+					hist[index][i] = args[i];
     				token = strtok(NULL, " ");
     			}
 			}
 			
-			args[i] = NULL;					
+			args[i] = NULL;
+			hist[histIndex][i] = NULL;			
+			index++;					
 			pid_t pid = fork();
 			
 			//check if child process
 			if (pid == 0) {
 				execvp(args[0], args);
-				printf("\n");
 			}
 			//check if parent process and ampersand found
 			else if (pid > 0 && ampersand == true) {
 				wait(NULL);
 			}
 		}
-			
-			//should_run = 0;
-	
-		/**
-		* After reading user input, the steps are:
-		* (1) fork a child process using fork()
-		* (2) the child process will invoke execvp()
-		* (3) if command included &, parent will invoke wait()
-		*/
 	}
 	return 0;
 }
